0d46e006cbcad75d66e075e0468b312c1e980b1c
Kai Patel <Kai.Patel@flextrade.com>
2024-12-19 02:24:50 +0000

    complete rework of lsp, cmp, theme, filetrees

--- nvim/.config/nvim/lua/user/brackets.lua
+++ nvim/.config/nvim/lua/user/brackets.lua
@@ -4,11 +4,4 @@ autopairs.setup {}
 autopairs.remove_rule("'")
 autopairs.remove_rule("\"")
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on(
-    'confirm_done',
-    cmp_autopairs.on_confirm_done()
-)
-
 require("sentiment").setup {}
---
+++ nvim/.config/nvim/lua/user/quickfix.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
