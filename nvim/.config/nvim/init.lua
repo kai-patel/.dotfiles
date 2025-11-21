5cf5d0a674567874a169ce3a9115e0ddb18ae194
Kai Patel <Kai.Patel@flextrade.com>
2025-11-21 09:59:45 +0000

    lsp fixes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -64,10 +64,10 @@ vim.opt.ttimeout = true
 vim.opt.ttimeoutlen = 50
 vim.keymap.set("n", "<leader>w", ":w !clip.exe<CR>", { silent = true })
 vim.keymap.set("v", "<leader>w", ":'<,'>w !clip.exe<CR>", { silent = true })
+vim.lsp.set_log_level("ERROR")
 
 require("config.lazy")
 require("user.telescope")
 require("user.oil")
 require("user.brackets")
 require("user.lsp")
-
