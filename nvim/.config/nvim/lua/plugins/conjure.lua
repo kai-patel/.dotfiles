33c57cfe926a8054e286733833798498fb5f66e0
Kai Patel <Kai.Patel@flextrade.com>
2026-04-30 13:47:55 +0100

    clj-only conjure, tmux fixes

--- nvim/.config/nvim/lua/plugins/conjure.lua
+++ nvim/.config/nvim/lua/plugins/conjure.lua
@@ -1,1 +1,7 @@
-return { "Olical/conjure" }
+return {
+    "Olical/conjure",
+    config = function()
+        -- `g:conjure#filetypes`
+        vim.api.nvim_set_var("conjure#filetypes", { "clojure" })
+    end
+}
