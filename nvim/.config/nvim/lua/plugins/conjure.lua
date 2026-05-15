9f9e1b8ffd7d2bcec147eb037ce64de04cf6e1c8
Kai Patel <Kai.Patel@flextrade.com>
2026-05-16 00:34:41 +0100

    fix: conjure config

--- nvim/.config/nvim/lua/plugins/conjure.lua
+++ nvim/.config/nvim/lua/plugins/conjure.lua
@@ -1,6 +1,6 @@
 return {
     "Olical/conjure",
-    config = function()
+    init = function()
         -- `g:conjure#filetypes`
         vim.api.nvim_set_var("conjure#filetypes", { "clojure" })
     end
